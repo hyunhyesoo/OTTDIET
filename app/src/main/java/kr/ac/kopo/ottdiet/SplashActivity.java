0c8522f91d3315ad7e9c78bdea0d98864ef8f810
hyunhyesoo <sue990831@gmail.com>
2026-06-24 09:04:04 +0900

    배경색 수정, 스플래시 화면 추가

---
+++ app/src/main/java/kr/ac/kopo/ottdiet/SplashActivity.java
@@ -0,0 +1,25 @@
+package kr.ac.kopo.ottdiet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Delay of 1.2 seconds (1200ms) before opening MainActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Close the splash screen so the user can't press back to it
+            }
+        }, 1200);
+    }
+}
